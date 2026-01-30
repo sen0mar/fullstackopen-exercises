f0dadeba8df911d45d3274b3c585da07898aa010
Omar <omar.ssenhaji@gmail.com>
2026-01-30 04:09:28 +0100

    Part2: Add exercises 2.4 2.5

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,19 +1,29 @@
-export default function Course({ course }) {
-  const sum = course.parts.reduce((acc, curr) => acc + curr.exercises, 0);
-  console.log(sum);
+export default function Course({ courses }) {
   return (
     <div>
-      <h1>{course.name}</h1>
-      <div>
-        {course.parts.map((part) => (
-          <p key={part.id}>
-            {part.name}: {part.exercises}
-          </p>
-        ))}
-      </div>
-      <p>
-        <strong>{`Total of ${sum} exercises`}</strong>
-      </p>
+      <h1>Web development curriculum</h1>
+
+      {courses.map((course) => {
+        const total = course.parts.reduce(
+          (sum, part) => sum + part.exercises,
+          0,
+        );
+
+        return (
+          <div key={course.id}>
+            <h2>{course.name}</h2>
+
+            {course.parts.map((part) => (
+              <p key={part.id}>
+                {part.name}
+                {part.exercises}
+              </p>
+            ))}
+
+            <strong>Total of {total} exercises</strong>
+          </div>
+        );
+      })}
     </div>
   );
 }
