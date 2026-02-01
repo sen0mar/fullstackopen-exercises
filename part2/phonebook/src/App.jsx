cc489e67620d8fea565eb37fd12ba9259aaa0d91
Omar <omar.ssenhaji@gmail.com>
2026-02-01 16:18:25 +0100

    Part2: Add exercises 2.16 2.17

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,12 +3,14 @@ import personService from "./service/personService";
 import PersonForm from "./components/PersonForm";
 import Filter from "./components/Filter";
 import Persons from "./components/Persons";
+import Notification from "./components/Notification";
 
 const App = () => {
   const [persons, setPersons] = useState([]); // List of users
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [search, setSearch] = useState("");
+  const [notification, setNotification] = useState(null);
 
   // Retrieve list of persons from server and display them
   useEffect(() => {
@@ -38,51 +40,76 @@ const App = () => {
       return;
     }
 
-    // Creates new person object for non existing person
-    const newPerson = {
-      name: newName,
-      number: newNumber,
-    };
-
     // Checks for an existing person
     const existingPerson = persons.find((p) => p.name === newName);
-    // Creates a new object with the updated number if the person exists
-    const updatedPerson = { ...existingPerson, number: newNumber };
-
     // If the person exists, ask a confirmation before updating
     if (existingPerson) {
       const ok = window.confirm(
         `${newName} already exists in the PhoneBook, replace the old number with a new one?`,
       );
       if (!ok) return;
+      // Creates a new object with the updated number if the person exists
+      const updatedPerson = { ...existingPerson, number: newNumber };
+
+      // Updates phone number in the server after checking if the person exists and asking for confirmation
+      personService
+        .updatePerson(existingPerson.id, updatedPerson)
+        .then((returnedPerson) => {
+          setPersons(
+            persons.map((p) =>
+              p.id === existingPerson.id ? returnedPerson : p,
+            ),
+          );
+          setNewName("");
+          setNewNumber("");
+
+          showNotification(
+            `Updated ${returnedPerson.name}'s number`,
+            "success",
+          );
+        })
+        .catch(() => {
+          showNotification(
+            `Information of ${existingPerson.name} has already been removed from server`,
+            "error",
+          );
+          setPersons(persons.filter((p) => p.id !== existingPerson.id));
+        });
+      return;
     }
+
+    // Creates new person object for non existing person
+    const newPerson = {
+      name: newName,
+      number: newNumber,
+    };
 
     // Creates new person in the server
     personService.createPerson(newPerson).then((returnedPerson) => {
       setPersons(persons.concat(returnedPerson));
       setNewName("");
       setNewNumber("");
+      showNotification(`Added ${returnedPerson.name}`, "success");
     });
-
-    // Updates phone number in the server after checking if the person exists and asking for confirmation
-    personService
-      .updatePerson(existingPerson.id, updatedPerson)
-      .then((returnedPerson) => {
-        setPersons(
-          persons.map((p) => (p.id === existingPerson.id ? returnedPerson : p)),
-        );
-        setNewName("");
-        setNewNumber("");
-      });
   };
 
   // Delete a person
   const deletePerson = (id, name) => {
-    if (window.confirm(`Are you sure you want to delete ${name} ?`)) {
-      personService.deletePerson(id).then(() => {
+    if (!window.confirm(`Are you sure you want to delete ${name} ?`)) return;
+    personService
+      .deletePerson(id)
+      .then(() => {
+        setPersons(persons.filter((p) => p.id !== id));
+        showNotification(`Deleted ${name}`, "success");
+      })
+      .catch(() => {
+        showNotification(
+          `Information of ${name} has already been removed from server`,
+          "error",
+        );
+
         setPersons(persons.filter((p) => p.id !== id));
       });
-    }
   };
 
   // Filter a person
@@ -93,9 +120,16 @@ const App = () => {
           person.name.toLowerCase().includes(search.toLowerCase()),
         );
 
+  // Show notification
+  const showNotification = (text, type = "success") => {
+    setNotification({ text, type });
+    setTimeout(() => setNotification(null), 3000);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification notification={notification} />
       <Filter search={search} handleSearch={handleSearch} />
       <PersonForm
         newName={newName}
---
+++ part2/phonebook/src/components/Notification.jsx
@@ -0,0 +1,7 @@
+const Notification = ({ notification }) => {
+  if (!notification) return null;
+
+  return <div className={notification.type}>{notification.text}</div>;
+};
+
+export default Notification;
